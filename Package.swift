0ddb895e738a8fbcd2b34875d63e3a3b2b45bc4b
Mohssen Fathi <mfathi@mercuryintermedia.com>
2016-10-07 09:08:24 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,12 @@
+import PackageDescription
+
+let package = Package(
+    name: "Rando",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0)
+
+    ])
+
+
---
+++ Sources/main.swift
@@ -0,0 +1,16 @@
+
+import Kitura
+import HeliumLogger
+
+HeliumLogger.use()
+
+let router = Router()
+
+router.get("/") {
+    request, response, next in
+    response.send("Hello, World!")
+    next()
+}
+
+Kitura.addHTTPServer(onPort: 8181, with: router)
+Kitura.run()
